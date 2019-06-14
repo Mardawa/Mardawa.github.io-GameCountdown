0bcceba73ef3bd5a2f3a2589cda587e3341b9c9b
840Sh33p <luuquocbao97@gmail.com>
2019-06-14 16:04:12 +0200

    Updates

---
+++ precache-manifest.05c3e13bdf5376d79399c4f998ab7211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ede5f86dedf99470da11",
+    "url": "/GameCountdown/static/js/runtime~main.a4f88fda.js"
+  },
+  {
+    "revision": "ee708745aa5aa62b7c9b",
+    "url": "/GameCountdown/static/js/main.87b13573.chunk.js"
+  },
+  {
+    "revision": "810f98c17f58f72cb1ad",
+    "url": "/GameCountdown/static/js/2.7f062e9a.chunk.js"
+  },
+  {
+    "revision": "810f98c17f58f72cb1ad",
+    "url": "/GameCountdown/static/css/2.76f814ac.chunk.css"
+  },
+  {
+    "revision": "a6151cfb3a85fc6afed05b44c1490528",
+    "url": "/GameCountdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27dd1a5229ffbfb5bc3218b1db1f2115.js"
+  "/GameCountdown/precache-manifest.05c3e13bdf5376d79399c4f998ab7211.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/GameCountdown/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
