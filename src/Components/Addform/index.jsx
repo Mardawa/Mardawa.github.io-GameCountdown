f784cc69b1e033baaf95c0645043cafeca2d2911
Sh33p <luuquocbao97@gmail.com>
2019-06-15 15:58:48 +0200

    Store games in external file

--- src/Components/Addform/index.jsx
+++ src/Components/Addform/index.jsx
@@ -6,6 +6,8 @@ import Grid from "@material-ui/core/Grid";
 import AddCountdownForm from "./Form/Addnewcountdown";
 import Countdown from "../Countdown";
 
+import { countdown } from "../../store";
+
 const styles = theme => ({
   root: {
     flexGrow: 1
@@ -25,79 +27,7 @@ const styles = theme => ({
 export default withStyles(styles)(
   class AddCountdown extends Component {
     state = {
-      countdown: [
-        {
-          name: "Mario Maker",
-          date: "2019-06-28",
-          url:
-            "https://static.fnac-static.com/multimedia/Images/FD/Comete/117569/CCP_IMG_ORIGINAL/1526953.jpg"
-        },
-        {
-          name: "Spyro",
-          date: "2019-09-03",
-          url:
-            "https://steamcdn-a.akamaihd.net/steam/apps/996580/header.jpg?t=1560272976"
-        },
-        {
-          name: "Borderlands 3",
-          date: "2019-09-13",
-          url:
-            "http://image.jeuxvideo.com/medias-md/155430/1554296862-6539-card.jpg"
-        },
-        {
-          name: "Link's awakening",
-          date: "2019-09-20",
-          url:
-            "http://image.jeuxvideo.com/medias-md/156027/1560274502-6067-card.jpg"
-        },
-        {
-          name: "Ghost recon",
-          date: "2019-10-04",
-          url:
-            "https://images.idgesg.net/images/article/2019/06/ghost-recon-breakpoint-cole-walker-100799252-large.jpg"
-        },
-        {
-          name: "Modern warfare",
-          date: "2019-10-25",
-          url:
-            "http://image.jeuxvideo.com/medias-md/156042/1560418602-5374-card.jpg"
-        },
-        {
-          name: "Star war",
-          date: "2019-11-15",
-          url:
-            "https://news.xbox.com/en-us/wp-content/uploads/SWFJO_XB1_PROMO_Wire_Hero_940x528-hero-1.jpg"
-        },
-        {
-          name: "Pokemon",
-          date: "2019-11-15",
-          url: "https://data.junkee.com/wp-content/uploads/2019/06/Wooloo.jpg"
-        },
-        {
-          name: "Gods and monster",
-          date: "2020-02-25",
-          url:
-            "https://dontfeedthegamers.com/wp-content/uploads/2019/06/gods-and-monsters-ubisoft-e3-2019-1.jpg"
-        },
-        {
-          name: "Watch dogs legion",
-          date: "2020-03-06",
-          url:
-            "https://ksassets.timeincuk.net/wp/uploads/sites/54/2019/06/watch-dogs-legion-gameplay-trailer-920x518.jpg"
-        },
-        {
-          name: "Animal crossing",
-          date: "2020-03-20",
-          url:
-            "https://static.mmzstatic.com/wp-content/uploads/2019/06/animal-crossing-new-horizons.jpg"
-        },
-        {
-          name: "Cyberpunk 2077",
-          date: "2020-04-16",
-          url:
-            "https://www.destructoid.com/ul/557252-cyberpunk-2077-e3-keanu.jpg"
-        }
-      ],
+      countdown,
       nextDate: "",
       nextName: "",
       nextUrl: ""
